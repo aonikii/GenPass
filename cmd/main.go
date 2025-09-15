d9cc1f91827c00fba7d5edee2be1fdbb9141fbb5
Andrey <andreyonikii@gmail.com>
2025-09-15 15:39:04 +0300

    добавил новый маршрут на страницу профиля

--- cmd/main.go
+++ cmd/main.go
@@ -23,6 +23,7 @@ func main() {
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) { auth.Register(w, r, tmpl) })
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) { user.Dashboard(w, r, tmpl) })
 	http.HandleFunc("/generate", func(w http.ResponseWriter, r *http.Request) { user.Generate(w, r, tmpl) })
+	http.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) { user.Profile(w, r, tmpl) })
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
 	log.Println("Сервер запущен на http://localhost:8080")
